42314fc8defa218297be553d172ea251b4e94182
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-05-26 10:07:40 +0530

    Time: 0 ms (100%), Space: 23.8 MB (78.07%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,11 +1,15 @@
 class Solution {  
 public:  
     void moveZeroes(vector<int>& nums) {  
-        int i = 0; 
-        for (int j = 0; j < nums.size(); j++) {  
-            if (nums[j] != 0) {  
-                swap(nums[i++], nums[j]);  
-            }  
-        }  
+      int zerotracker=0;
+      int nonzero=0;
+
+       for(int nonzero=0;nonzero<nums.size();nonzero++){
+        if(nums[nonzero]!=0){
+            swap(nums[nonzero], nums[zerotracker]);
+            zerotracker++;
+        }
+       }
+       
     }  
 };  
