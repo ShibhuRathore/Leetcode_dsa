375e3a32d9faeaf4be426dac4ed22dd7fdcb77ad
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-05-28 09:29:42 +0530

    Time: 59 ms (8.54%), Space: 75 MB (56.22%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -8,7 +8,7 @@ public:
     unordered_map<int,int>mp;
     while(right<n){
         mp[fruits[right]]++;
-        while(mp.size()>2){
+        if(mp.size()>2){
             mp[fruits[left]]--;
             if(mp[fruits[left]]==0){
                 mp.erase(fruits[left]);
