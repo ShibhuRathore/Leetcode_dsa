9edc9d148aaf680cdcb746b80b99ad6a3d969a66
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-05-31 09:14:40 +0530

    Time: 35 ms (89.66%), Space: 46.7 MB (41.17%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,12 +3,13 @@ public:
     int subarraySum(vector<int>& nums, int k) {
       int n=nums.size();
       vector<int> prefix(n,0);
-      prefix[0]=nums[0];
       int count=0;
+      prefix[0]=nums[0];
       for(int i=1;i<n;i++){
         prefix[i]=prefix[i-1]+nums[i];
-      }  
+      }
       unordered_map<int,int>mp;
+      int j=0;
       for(int j=0;j<n;j++){
         int val=prefix[j]-k;
         if(prefix[j]==k){
@@ -17,10 +18,8 @@ public:
         if(mp.find(val)!=mp.end()){
             count+=mp[val];
         }
-        if(mp.find(prefix[j])==mp.end()){
-            mp[prefix[j]]=0;
-        }
-        mp[prefix[j]]++;
+        
+         mp[prefix[j]]++;
       }
       return count;
     }
