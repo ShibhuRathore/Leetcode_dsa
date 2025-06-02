4d5330ddc3467343a93458669a8bb14a384621f4
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-06-02 09:03:32 +0530

    Time: 1 ms (70.94%), Space: 11.9 MB (51.89%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        int rm = ransomNote.size();
-        int mg = magazine.size();
-        sort(ransomNote.begin(), ransomNote.end());
-        sort(magazine.begin(), magazine.end());
-        int r = 0;
-
-        for (int m = 0; m < mg && r < rm; m++) {
-            if (ransomNote[r] == magazine[m]) {
-                r++;
-            }
-        }
-        if (r == rm) {
-            return true;
-        } else {
+     int m=ransomNote.length();
+     int n=magazine.length();
+     unordered_map<char,int> mp;
+     for(auto &ch:magazine){
+        mp[ch]++;
+     }
+     for(auto &ch:ransomNote){
+        if(mp[ch]==0){
             return false;
         }
+        mp[ch]--;
+     }
+     return true;
     }
 };
