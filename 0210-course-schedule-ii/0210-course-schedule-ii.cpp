40e90b11debf219e43b7200d5cdfecf1099496de
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-06-19 19:03:03 +0530

    Time: 5 ms (44.97%), Space: 18.4 MB (51.59%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
+
+   void DFS(vector<vector<int>>&adj, int u, vector<bool>&vis, vector<bool>&inrecursion,stack<int>&st){
+      vis[u]=true;
+      inrecursion[u]=true;
+      for(auto &v:adj[u]){
+        if(vis[v]==false){
+            DFS(adj,v,vis,inrecursion,st);
+        }
+        else if(inrecursion[v]==true){
+            return;
+        }
+      }
+      st.push(u);
+      inrecursion[u]=false;
+      
+   }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         int n=numCourses;
         vector<vector<int>>adj(n);
-        vector<int>indegree(n);
+        vector<bool>inrecursion(n,false);
+        stack<int>st;
+        vector<bool>vis(n,false);
         for(auto &vec:prerequisites){
             int u=vec[1];
             int v=vec[0];
             adj[u].push_back(v);
-            indegree[v]++;
         }
-        queue<int>q;
-        vector<int>res;
         for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                q.push(i);
+            if(vis[i]==false){
+                DFS(adj,i,vis,inrecursion,st);
             }
         }
-
-        while(!q.empty()){
-            int source=q.front();
-            q.pop();
-            res.push_back(source);
-            for(auto &v:adj[source]){
-                indegree[v]--;
-             if(indegree[v]==0){
-                q.push(v);
-             }
-            }
+        vector<int>res;
+        while(!st.empty()){
+           res.push_back(st.top()) ;
+           st.pop();
         }
-        if (res.size() != n) {
+        if(res.size()!=n){
             return {};
         }
         return res;
