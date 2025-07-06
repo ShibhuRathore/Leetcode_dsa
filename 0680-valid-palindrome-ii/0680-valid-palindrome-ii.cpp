98a71a43c30fd493a8c2e617ba55184e6249ebf9
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-07-06 11:52:33 +0530

    Time: 7 ms (36.37%), Space: 22.2 MB (56.07%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    bool checkPalindrome(string &s, int i, int j){
+        while(i<j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+       int i=0;
+       int j=s.length()-1;
+       while(i<j){
+        if(s[i]==s[j]){
+            i++;
+            j--;
+        }
+        else{
+          bool left=  checkPalindrome(s,i,j-1);
+          bool right=  checkPalindrome(s,i+1,j);
+          return left||right;
+        }
+       } 
+       return true;
+    }
+};
