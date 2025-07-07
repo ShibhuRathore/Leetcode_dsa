cdc2964a12e779ab9459d30a641ec8cd9ce823c8
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-07-07 14:00:25 +0530

    Time: 0 ms (100%), Space: 14.3 MB (33.54%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,21 +1,21 @@
 class Solution {
-public:
-   void solve(int curr, int n , vector<int>&result){
-        if(curr>n){return ;}
-        result.push_back(curr);
+    public:
+    void solve( int i,int n,vector<int>&result){
+        if (i>n){
+            return;
+        }
+        result.push_back(i);
         for(int append=0;append<=9;append++){
-            int newnum=curr*10+append;
-            if(newnum>n)
-                return;          
-            solve(newnum,n,result);
+            int newnum=10*i+append;
+            if(newnum<=n){
+                solve(newnum,n,result);
+            }
         }
-        
-   }
-    vector<int> lexicalOrder(int n) {
-        vector<int> result;
-        for(int i=1;i<=9;i++){
-            solve(i,n,result);
-        }
-        return result;
     }
+    vector<int> lexicalOrder(int n) {
+     vector<int>result;
+    for( int i=1;i<=9;i++){
+        solve(i,n,result);
+    }
+    return result;}
 };
