2f7c0bca37e38c3c09df242a3b63aa4176d7d0e1
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-07-24 22:33:23 +0530

    Time: 40 ms (72.87%), Space: 46.7 MB (40.98%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,14 +1,33 @@
+// prefix sum nikalo
+//map mei prefixsum ki frequencies store karo but initially alag loop me beforehand mat karna usese future wale prefixsum ki frequencies bhi stoer ho jayengi 
+// diagram yaad karo prefix, pefix-k,k wala 
+// fir solve kardo
+
+
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-      int count=0;
-     for(int i=0;i<nums.size();i++){
-        int sum=0;
-        for(int j=i;j<nums.size();j++){
-            sum+=nums[j];
-            if(sum==k)count++;
-        }
+        int n=nums.size();
+     vector<int>prefix(n,0);
+     prefix[0]=nums[0];
+     unordered_map<int,int>mp;
+     mp[0]=1;
+     for(int i=1;i<nums.size();i++){
+        prefix[i]=prefix[i-1]+nums[i];
      }
-     return count;
+    
+     int count=0;
+     int j=0;
+     while(j<prefix.size()){
+        int sum=prefix[j]-k;
+        if(mp.find(sum)!=mp.end()){
+            count+=mp[sum];
+        }
+        mp[prefix[j]]++;
+        j++;
+     }
+
+return count;
+
     }
 };
