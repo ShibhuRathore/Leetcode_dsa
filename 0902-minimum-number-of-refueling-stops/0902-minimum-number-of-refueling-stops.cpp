393ef9ed4345c42782c0e0b7de4dacd727d51f21
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-07-27 19:43:09 +0530

    Time: 0 ms (100%), Space: 20.2 MB (77.12%) - LeetHub

--- 0902-minimum-number-of-refueling-stops/0902-minimum-number-of-refueling-stops.cpp
+++ 0902-minimum-number-of-refueling-stops/0902-minimum-number-of-refueling-stops.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
-        sort(stations.begin(),stations.end());
-        priority_queue<int> pq;
-        int fuel=startFuel;
-        int i=0;
-        int refuel=0;
-        while(fuel<target){
-            while(i<stations.size()&&fuel>=stations[i][0]){
-                pq.push(stations[i][1]);
-                i++;
-            }
-
-            if(pq.empty()){
-                return -1;
-            }
-              fuel+=pq.top();
-              pq.pop();
-              refuel++;
+    //    sort(stations.begin(),stations.end());
+       priority_queue<int>pq;
+       int n=stations.size();
+       int fuel=startFuel;
+       int refuel=0;
+       int i=0;
+       while(fuel<target){
+        while(i<n&&stations[i][0]<=fuel){
+            pq.push(stations[i][1]);
+            i++;
         }
-        return refuel;
-
+        if(pq.empty())return -1;
+       fuel+= pq.top();
+       pq.pop();
+       refuel++;
+       }
+       return refuel;
     }
 };
