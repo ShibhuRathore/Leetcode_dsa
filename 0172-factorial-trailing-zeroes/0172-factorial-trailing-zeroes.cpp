895b80af7b06d1b17bbd7fe038356b034546f844
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-07-30 22:03:44 +0530

    Time: 0 ms (100%), Space: 8.1 MB (85.75%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int res = 0;
-        while (n > 0) {
-            n /= 5; // Count multiples of 5, 25, 125, etc.
-            res += n;
-        }
-        return res;
+     int res=0;
+     while(n>0){
+        n/=5;
+        res+=n;
+     }
+     return res;
     }
 };
