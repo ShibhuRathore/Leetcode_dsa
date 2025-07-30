e6ff429f803d4acfb4b83814c1a54aee2fd111c6
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-07-30 19:56:23 +0530

    Time: 0 ms (100%), Space: 87.2 MB (45.64%) - LeetHub

--- 0790-global-and-local-inversions/0790-global-and-local-inversions.cpp
+++ 0790-global-and-local-inversions/0790-global-and-local-inversions.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     bool isIdealPermutation(vector<int>& nums) {
         int n = nums.size();
-        int minRight = n; // Minimum value from the right side
-        
-        for (int i = n - 1; i >= 2; i--) {
-            minRight = min(minRight, nums[i]);
-            if (nums[i - 2] > minRight) {
-                return false;
-            }
+        int minright = nums[n-1]; // Minimum value from the right side
+        for(int i=n-1;i>=2;i--){
+            minright=min(minright,nums[i]);
+            if(minright<nums[i-2])return false;
         }
-        return true;
+       return true;
     }
 };
