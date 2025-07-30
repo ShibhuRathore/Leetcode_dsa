1f150f1300ed86a66aeb9f3786aa76688d39177b
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-07-30 23:01:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int s=1;
+        int e=x;
+        int mid=s+(e-s)/2;
+        int ans=0;
+        while(s<e){
+            if(mid*mid==x)return mid;
+            else if (mid*mid<x) {
+                 s=mid+1;
+                 ans=mid;
+            }
+            else{
+             
+                e=mid-1;
+            }
+            mid=s+(e-s)/2;
+        }
+        return ans;
+    }
+};
