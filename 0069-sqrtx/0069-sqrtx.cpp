56dfe5731c0836f177af6073eb4555609772fcf0
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-07-30 23:03:29 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.42%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -3,9 +3,9 @@ public:
     int mySqrt(int x) {
         int s=1;
         int e=x;
-        int mid=s+(e-s)/2;
+        long   mid=s+(e-s)/2;
         int ans=0;
-        while(s<e){
+        while(s<=e){
             if(mid*mid==x)return mid;
             else if (mid*mid<x) {
                  s=mid+1;
