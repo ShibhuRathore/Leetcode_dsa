7ddb759cfc9acc32c10ae61a9dc65c608a24dfe8
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-08-02 07:30:02 +0530

    Time: 0 ms (100%), Space: 8.7 MB (48.44%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -1,12 +1,13 @@
 class Solution {
 private:
-    long helper(string &s, int idx) {
-        if (!idx)
-            return s[idx] - 'A' + 1;
-        return 26 * helper(s, idx - 1) + s[idx] - 'A' + 1;
-    }
+ 
 public:
     int titleToNumber(string columnTitle) {
-        return helper(columnTitle, columnTitle.size() - 1);
+      long result=0;
+      for(auto &ch:columnTitle){
+            int value=ch-'A'+1;
+            result=result*26+value;
+      }
+      return result;
     }
 };
