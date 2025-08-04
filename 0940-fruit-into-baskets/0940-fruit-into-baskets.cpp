368b73ba9778175ac6030ec1cf0545be0f2f68dc
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-08-04 08:02:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int totalFruit(vector<int>& nums) {
-      unordered_map<int,int>mp;
-     int maxlen=INT_MIN;
-      int i=0;
-      int j=0;
-      while(j<nums.size()){
-         mp[nums[j]]++;
-         if(mp.size()>2){
-              while(mp.size()>2){
-                mp[nums[i]]--;
-              
-                if(mp[nums[i]]==0){
-                    mp.erase(nums[i]);
-                }
-                  i++;
-              }
-            
-         }
-           maxlen=max(maxlen,j-i+1);
-              j++;
-      }
-      return maxlen;
+    int i=0;
+    int j=0;
+    unordered_map<int,int>mp;
+    int maxlen=INT_MIN;
+    while(j<nums.size()){
+        while(mp.size()>2){
+            mp[nums[i]]--;
+            if(mp[nums[i]]==0){mp.erase(nums[i]);}
+            i++;
+        }
+        mp[nums[j]]++;
+        maxlen=max(maxlen,j-i+1);
+        j++;
+    }
+    return maxlen;
     }
 };
