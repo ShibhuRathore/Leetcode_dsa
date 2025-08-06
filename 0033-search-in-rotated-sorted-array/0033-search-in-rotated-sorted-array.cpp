ac50c18f642d421a02b925c6bf348adc694cff7b
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-08-06 10:21:10 +0530

    Time: 0 ms (100%), Space: 15.3 MB (32.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int s=0;
-        int e=nums.size()-1;
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            if (nums[mid] == target)
-                return mid;
-            // left array is sorted 
-            if(nums[s]<=nums[mid]){
-                //target is present in the left part
-                // extra condition nums[s]<=target islie lagani pad rahi h ki left part< target warna agar extreme left hi bada ho jayega to left part mei target hoga hi nahi
-                if(nums[mid]>target && nums[s]<=target){
-                    e=mid-1;                }
-                else{
-                    s=mid+1;
-                }
-            }
-            //right array is sorted
-            else{
-                // target is present in the right part
-               if(nums[mid]<target && nums[e]>=target){
-                s=mid+1;
-               }
-               else{
+      int s=0;
+       int e=nums.size()-1;
+       while(s<=e){
+        int mid=s+(e-s)/2;
+        if(nums[mid]==target)return mid;
+        if(nums[s]<=nums[mid]){
+            if(nums[s]<=target&&target<=nums[mid]){
                 e=mid-1;
-               }
+            }
+            else{
+                s=mid+1;
             }
         }
-        return -1;
+        else{
+            if(nums[mid]<=target&&target<=nums[e]){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+       }
+       return -1;
     }
 };
