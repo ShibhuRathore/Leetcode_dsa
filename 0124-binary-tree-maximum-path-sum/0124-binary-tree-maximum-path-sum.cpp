bdb269aee90537598cb6d19af912afe6769cb914
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-10-18 23:24:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -13,14 +13,14 @@ class Solution {
 public:
     int maxsum=INT_MIN;
     int solve(TreeNode*root){
-     if(!root)return 0;
-     int lh=solve(root->left);
-     int rh=solve(root->right);
-     int neeche_ka_ans=root->val+lh+rh;
-     int best_ans=max(lh,rh)+root->val;
-     int root_ans=root->val;
-     maxsum=max(max(neeche_ka_ans,best_ans),max(root_ans,maxsum));
-   return max(best_ans,root_ans);;
+       if (!root)return 0;
+       int lh=solve(root->left);
+       int rh=solve(root->right);
+       int best_ans=max(lh,rh)+root->val;
+       int neeche_ans=root->val+lh+rh;
+       int root_ans=root->val;
+       maxsum=max(max(root_ans,neeche_ans),max(best_ans,maxsum));
+       return max(neeche_ans,root_ans);
     }
     int maxPathSum(TreeNode* root) {
         solve(root);
