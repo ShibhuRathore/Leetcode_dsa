ca8d626bfae266f86507d982a55fa78de655997c
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-11-01 12:20:04 +0530

    Time: 15 ms (31.25%), Space: 70.7 MB (61.58%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
   bool find132pattern(vector<int>& nums) {
-    int n = nums.size();
-    int second = INT_MIN; // this stores the "2" in 132
-    stack<int> st; // used to keep possible "3" values
-
-    // Traverse from right to left
-    for (int i = n - 1; i >= 0; i--) {
-        // if we find a number smaller than "second", we found pattern
-        if (nums[i] < second) {
-            return true;
-        }
-
-        // keep popping while current > top, updating "second"
-        while (!st.empty() && nums[i] > st.top()) {
-            second = st.top();
+      int third=INT_MIN;
+      int n=nums.size();
+      stack<int>st;
+      for(int i=n-1;i>=0;i--){
+        if(nums[i]<third)return true;
+         while(!st.empty()&&st.top()<nums[i]){
+            third=st.top();
             st.pop();
-        }
-
-        // push current number as a possible "3"
-        st.push(nums[i]);
-    }
-
-    return false; 
+         }
+         st.push(nums[i]);
+      }
+      return false;
     }
 };
