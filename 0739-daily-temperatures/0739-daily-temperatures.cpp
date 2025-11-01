881f8497e8978ac3a173de15e1d097cfd0f1049e
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-11-01 09:01:21 +0530

    Time: 24 ms (49.41%), Space: 107.4 MB (23.98%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<int>st;
         int n = temperatures.size();
-        vector<int> result(n, 0);
+        vector<int> ans(n, 0);
+        stack<int> st;  // stores indices (monotonic decreasing stack)
 
-        for(int i=temperatures.size()-1;i>=0;i--){
-            while(!st.empty()&&temperatures[i]>=temperatures[st.top()]){
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                int idx = st.top();
                 st.pop();
-            }
-            if(!st.empty()){
-                result[i]=st.top()-i;
+                ans[idx] = i - idx;  // number of days to wait
             }
             st.push(i);
         }
-        return result;
+
+        return ans;
     }
 };
