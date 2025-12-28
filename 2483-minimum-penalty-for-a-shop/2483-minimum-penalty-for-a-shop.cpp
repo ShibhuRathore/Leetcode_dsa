9a53fc35c43a25379214edabfb53ba8f4e017d21
Shibhu Rathore <shibhurathore7914@gmail.com>
2025-12-28 16:44:11 +0530

    Time: 0 ms (100%), Space: 12.4 MB (96.65%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,19 +1,41 @@
 class Solution {
 public:
     int bestClosingTime(string& customers) {
-        int n = customers.size();
-    
-        int penalty=0;//without adding y_rem does not matter 
-        int minP=penalty, minI=0;
-    
-        for (int i=1; i<=n; i++) {
-            int y=customers[i-1]=='Y'?1:-1;
-            penalty-=y;
-            if (minP>penalty) {
-                minP=penalty;
-                minI=i;
+        // int penalty =INT_MAX;
+        // int ans=INT_MAX;
+        // int close=0;
+        // while(close<=customers.size()){
+        //     int current_penalty=0;
+        //     // till close 
+        //     for(int i=0;i<close;i++){
+        //         if(customers[i]=='N'){current_penalty++;}
+        //     }
+        //     // after close ;
+        //     for(int i=close;i<customers.size();i++){
+        //         if(customers[i]=='Y'){current_penalty++;}
+        //     }
+        //     if(current_penalty<penalty){
+        //         penalty=current_penalty;
+        //         ans=close;
+        //     }
+        //     close++;
+        // }
+        // return ans;
+        int currpenalty=0;
+        int minpenalty=0;
+        int ans=0;
+        int close=0;
+        while(close<customers.size()){
+            if(customers[close]=='Y')currpenalty--;
+            else{
+                currpenalty++;
             }
+            if(currpenalty<minpenalty){
+                minpenalty=currpenalty;
+                ans=close+1;
+            }
+            close++;
         }
-        return minI;
+        return ans;
     }
 };
