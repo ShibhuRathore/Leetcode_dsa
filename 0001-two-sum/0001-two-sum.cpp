e6467738157e723baa931613c21843a80548cd03
Shibhu Rathore <shibhurathore7914@gmail.com>
2026-01-07 22:40:41 +0530

    Time: 3 ms (67.62%), Space: 14.8 MB (37.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int> &nums, int target) {
-        vector<pair<int,int>> numswithindex;
-        for(int i =0;i<nums.size();i++){
-            numswithindex.push_back({nums[i],i});
-        }
-        sort(numswithindex.begin(),numswithindex.end());
-      int i=0;
-      int j=nums.size()-1;
-          vector<int> ans;
-      while(i<j){
-       int sum=numswithindex[i].first+numswithindex[j].first;
-       if(sum<target){
-        i++;
-       } 
-       else if(sum>target){
-        j--;
-       }
-       else{
+     // brute force 
+    //  for(int i=0;i<nums.size()-1;i++){
+    //     for(int j=i+1;j<nums.size();j++){
+    //         if(nums[i]+nums[j]==target)return{i,j};
+    //     }
+    //  } 
+    //  return {-1,-1}; TC---->O(n^2). SC----->O(1)
     
-        ans.push_back(numswithindex[i].second);
-        ans.push_back(numswithindex[j].second);
-        return ans;
-       }
-      }
-      return ans;
+   
+
+    // with sorting + pair of indices and element 
+    // vector<pair<int,int>>vec(nums.size());
+    // for(int i =0;i<nums.size();i++){
+    //     vec[i]={nums[i],i};   //{element,indices}
+    // }
+    // sort(vec.begin(),vec.end());
+    // int i=0;
+    // int j=nums.size()-1;
+    // while(i<j){
+    //     if(vec[i].first+vec[j].first==target)return {vec[i].second,vec[j].second};
+    //     else if(vec[i].first+vec[j].first<target){i++;}
+    //     else{j--;}
+    // }
+    // return {-1,-1};  ------>Tc->O(NlogN)and SC--->O(N)
+
+     // with map and complement 
+    unordered_map<int,int>mp;
+    for(int i=0;i<nums.size();i++){
+        int complement=target-nums[i];
+        if(mp.find(complement)!=mp.end()){
+            return {i,mp[complement]};
+        }
+        mp[nums[i]]=i;
+    }
+       return {-1,-1}; // -----> TC->O(N) and map lookups are O(1) and SC->O(N)
     }
 };
