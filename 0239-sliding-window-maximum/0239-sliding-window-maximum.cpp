51d1bd9dd18a14e9b87b397d6677984b0a8585fd
Shibhu Rathore <shibhurathore7914@gmail.com>
2026-01-09 09:49:04 +0530

    Time: 9 ms (97.96%), Space: 139.2 MB (66.36%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,20 +1,18 @@
+// step1-> make space for incoming element in the window size of k
+// step2-> if the new coming element is greater than dq.back() remove it by using while loop
+// step3-> insert the element
+// step4-> if i>=k-1 start pushing the front of deque into the result 
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-      deque<int>dq;
-      vector<int>res;
-      for(int i=0;i<nums.size();i++){
-        if(!dq.empty()&&dq.front()<=i-k){
-            dq.pop_front();
+        deque<int>dq;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            while(!dq.empty()&&dq.front()<=i-k)dq.pop_front();
+            while(!dq.empty()&&nums[i]>=nums[dq.back()]) dq.pop_back();
+            dq.push_back(i);
+            if(i>=k-1)ans.push_back(nums[dq.front()]);
         }
-        while(!dq.empty()&&nums[dq.back()]<=nums[i]){
-            dq.pop_back();
-        }
-        dq.push_back(i);
-       if(i>=k-1){
-        res.push_back(nums[dq.front()]);
-       }
-      }
-      return res;
+        return ans;
     }
 };
