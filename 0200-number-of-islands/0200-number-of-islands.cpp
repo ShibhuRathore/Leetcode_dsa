8982d1e752896a89154ae2d24053046d93cbaf39
Shibhu Rathore <shibhurathore7914@gmail.com>
2026-01-16 10:39:52 +0530

    Time: 32 ms (25.73%), Space: 16.6 MB (64.48%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,50 +1,35 @@
 class Solution {
 public:
 
- bool isSafe(int newx,int newy,vector<vector<char>>&grid,vector<vector<bool>>&vis){
-    if(newx>=0&&newy>=0&&newx<grid.size()&&newy<grid[0].size()&&grid[newx][newy]=='1'&&vis[newx][newy]==0){
-        return true;
-    }
-    else{
-        return false;
-    }
- }
- void BFS(vector<vector<char>>&grid,vector<vector<bool>>&vis,int i , int j){
-     int m=grid[0].size();
-     int n=grid.size();
-    queue<pair<int,int>>q;
-    q.push({i,j});
+bool isSafe(vector<vector<char>>&grid,int i,int j, vector<vector<bool>>&vis){
+    int m=grid.size();
+    int n=grid[0].size();
+    if(i<0||i>=m||j<0||j>=n||grid[i][j]=='0'||vis[i][j]==true)return false;
+    return true;
+}
+ void dfs(vector<vector<char>>&grid,int i, int j,vector<vector<bool>>&vis){
     vis[i][j]=true;
-    while(!q.empty()){
-        auto src=q.front();
-        q.pop();
-        int x=src.first;
-        int y=src.second;
-        int dx[]={-1,0,1,0};
-        int dy[]={0,1,0,-1};
-        for(int k=0;k<4;k++){
-            int nx=x+dx[k];
-            int ny=y+dy[k];
-            if(isSafe(nx,ny,grid,vis)){
-                q.push({nx,ny});
-                vis[nx][ny]=true;
-            }
-        }
+    int dx[]={1,-1,0,0};
+    int dy[]={0,0,-1,1};
+    for(int k=0;k<4;k++){
+        int n_x=i+dx[k];
+        int n_y=j+dy[k];
+        if(isSafe(grid,n_x,n_y,vis))dfs(grid,n_x,n_y,vis);
     }
+    
  }
  int numIslands(vector<vector<char>>& grid) {
      int count=0;
      int m=grid.size();
      int n=grid[0].size();
      vector<vector<bool>>vis(m,vector<bool>(n,false));
-
      for(int i=0;i<m;i++){
-       for(int j=0;j<n;j++){
-        if(vis[i][j]==false&&grid[i][j]=='1'){
-            BFS(grid,vis,i,j);
-            count++;
+        for(int j=0;j<n;j++){
+            if(grid[i][j]=='1'&&!vis[i][j]){
+                dfs(grid,i,j,vis);
+                count++;
+            }
         }
-       }
      }
      return count;
     }
