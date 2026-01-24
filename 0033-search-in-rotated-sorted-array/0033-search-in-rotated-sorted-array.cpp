a4a087ca773fb9c0cb97feb6ff74be5c49f2945f
Shibhu Rathore <shibhurathore7914@gmail.com>
2026-01-24 09:42:48 +0530

    Time: 4 ms (0.9%), Space: 15.2 MB (65.69%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,27 +4,27 @@ public:
      int s=0;
      int e=nums.size()-1;
      while(s<=e){
-        int mid=s+(e-s)/2;
-        if(nums[mid]==target)return mid;
-        if(nums[s]<=nums[mid]){//left part sorted h 
-            if(nums[s]<=target&&target<=nums[mid]){
-                e=mid-1;
-            }
-            else{
-                s=mid+1;
-            }
-        }
-        else {
-          
-                if(nums[mid]<=target&&target<=nums[e]){
-                    s=mid+1;
-                }
-                else{
-                    e=mid-1;
-                }
-            
-        }
+     int mid=s+(e-s)/2;
+     if(nums[mid]==target){
+        return mid;
      }
+     else if (nums[mid]>=nums[s])//left hand is sorted
+     {
+        if(target<nums[mid]&&target>=nums[s]){
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+     } 
+     else{
+        if(target>nums[mid]&&target<=nums[e]){
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+     }}
      return -1;
     }
 };
