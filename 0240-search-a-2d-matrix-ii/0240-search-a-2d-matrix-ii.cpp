362b2f38d48497c85b0000b7c85365cd2ec5a94b
Shibhu Rathore <shibhurathore7914@gmail.com>
2026-01-25 17:51:56 +0530

    Time: 52 ms (64.03%), Space: 18.6 MB (91.57%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,23 +1,20 @@
+// young tableau format 
 class Solution {
 public:
   bool searchMatrix(vector<vector<int>>& matrix, int target) {
-  int m=matrix.size();
-  int n=matrix[0].size();
-  int row=0;
-  int col=n-1;
-  while(row<m&&col>=0){
-    if(matrix[row][col]==target){
-        return true;
+    int m=matrix.size();
+    int n=matrix[0].size();
+    int row=0;
+    int col=n-1;
+    while(row<m&&col>=0){
+        if(matrix[row][col]==target)return true;
+        else if (matrix[row][col]<target){
+            row++;
+        }
+        else{
+            col--;
+        }
     }
-    else if(matrix[row][col]<target){
-        row++;
-    }
-    else{
-        col--;
-    }
-  }
-  
-  return false;
-  
+    return false;
   }
 };
