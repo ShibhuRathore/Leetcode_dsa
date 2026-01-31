98f49337e4d2d94a80ac73281998a63c628c03eb
Shibhu Rathore <shibhurathore7914@gmail.com>
2026-01-31 11:43:49 +0530

    Time: 0 ms (100%), Space: 112.4 MB (59.1%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,22 +1,21 @@
+//  If (Sum of all Gas) - (Sum of all Cost) >= 0, and you found a starting point that can reach the end of the array without hitting zero, it is mathematically impossible to fail the remaining part of the circle.
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int totalGas = 0, totalCost = 0;
-        int start = 0;
-        int fuel = 0;
-
-        for (int i = 0; i < gas.size(); i++) {
-            totalGas += gas[i];
-            totalCost += cost[i];
-            fuel += gas[i] - cost[i];
-
-            // if we can't reach the next station, reset start
-            if (fuel < 0) {
-                start = i + 1;
-                fuel = 0;
-            }
+      int totalgas=0;
+      int totalcost=0;
+      int fuel=0;
+      int start=0;
+      int n=gas.size();
+      for(int i=0;i<n;i++){
+        totalgas+=gas[i];
+        totalcost+=cost[i];
+        fuel=fuel+gas[i]-cost[i];
+        if(fuel<0){
+            fuel=0;
+            start=i+1;
         }
-
-        return (totalGas < totalCost) ? -1 : start;
+      }
+      return totalgas>=totalcost?start:-1;
     }
 };
