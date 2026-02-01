f1ceef05f6d029660692d3de8b9826d810f822c3
Shibhu Rathore <shibhurathore7914@gmail.com>
2026-02-01 14:49:58 +0530

    Time: 0 ms (100%), Space: 11.3 MB (37.83%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -2,24 +2,41 @@ class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
         if (!head || left == right) return head;
-        
-        ListNode dummy(0);
-        dummy.next = head;
-        ListNode* prev = &dummy;
-        
-        for (int i = 0; i < left - 1; ++i) {
-            prev = prev->next;
+
+        ListNode *temp = head;
+        ListNode *prevtotemp = nullptr; // Initialize as null
+
+        // 1. Move to the starting position
+        for (int i = 1; i < left; i++) {
+            prevtotemp = temp;
+            temp = temp->next;
+        }
+
+        // 2. Setup reversal
+        ListNode *prev = nullptr;
+        ListNode *curr = temp;
+        ListNode *next = nullptr;
+        ListNode *tailOfSublist = temp; // 'temp' is the original 'left' node
+
+        // 3. Reverse (right - left + 1) nodes
+        for (int i = 0; i < (right - left + 1); i++) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // 4. Stitch back together
+        // 'prev' is now the new head of the reversed section
+        // 'curr' is the first node of the remaining list
+        if (prevtotemp != nullptr) {
+            prevtotemp->next = prev;
+        } else {
+            head = prev; // Update head if left was 1
         }
         
-        ListNode* current = prev->next;
-        
-        for (int i = 0; i < right - left; ++i) {
-            ListNode* next_node = current->next;
-            current->next = next_node->next;
-            next_node->next = prev->next;
-            prev->next = next_node;
-        }
-        
-        return dummy.next;
+        tailOfSublist->next = curr; // Connect tail to the rest of the list
+
+        return head;
     }
 };
